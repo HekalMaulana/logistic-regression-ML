70980641fbf3205d11b1fbeceac9d2257b212f54
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-26 11:35:30 +0800

    Splitting dataset into training set and test set

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+# splitting dataset into training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, random_state=0)
