140a33083b714527e16e8bb5642d979b48fc1c59
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-26 11:46:44 +0800

    Apllying Feature Scaling for training set and test set

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ y = dataset.iloc[:, -1].values
 # splitting dataset into training set and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
